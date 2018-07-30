7d6a575e9c15b1692b3ec8fe546e23442c061ab5
Manoj002 <ymanoj821@gmail.com>
2018-07-31 00:39:24 +0530

    00:39

---
+++ functions.sh
@@ -0,0 +1,25 @@
+greet() {
+    echo "hey"
+    echo "Hello, World!"
+    echo "$1, World!"
+}
+
+greet
+greet "Howdy"
+greeting=$(greet "Howdy") #command substition syntax
+
+echo "The greeting is $greeting!"
+
+
+global=123   #global_variable
+
+test() {
+    echo "global = $global"
+    local local_var="im a local variable"
+    echo "local_var = $local_var"
+}
+
+test
+
+echo "global = $global"
+echo "local_var = $local_var"
